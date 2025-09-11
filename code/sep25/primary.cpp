4dcfbe19e445e3dcc9d4e0d99ee392cd530c6ea4
guan <guan.wenj@qq.com>
2025-09-11 09:34:13 +0800

    可见reserve的预分配作用相当大

--- code/sep25/primary.cpp
+++ code/sep25/primary.cpp
@@ -1,3 +1,30 @@
 //
 // Created by guan on 2025/8/30.
 //
+
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+int main() {
+    auto start1 = std::chrono::high_resolution_clock::now();
+    std::vector<int> vec1;
+    for (int i=0;i<100000000;i++) {
+        vec1.push_back(i*3);
+    }
+    auto end1= std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(end1-start1);
+    std::cout <<"没reserve:"<< duration1.count() << std::endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::vector<int> vec;
+    vec.reserve(1000000);
+    for (int i=0;i<100000000;i++) {
+        vec.push_back(i*3);
+    }
+    auto end= std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+    std::cout <<"reserve:"<< duration.count() << std::endl;
+
+}
