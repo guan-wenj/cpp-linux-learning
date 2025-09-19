8cb065d9018e16fab5fbeb633975447db4c5cc9f
guan <guan.wenj@qq.com>
2025-09-19 16:12:17 +0800

    完成第二章

--- code/oct25/init_list.cpp
+++ code/oct25/init_list.cpp
@@ -11,9 +11,31 @@ class Student {
     void introduce(){
         std::println("hello, my name is {}, {} years old, i im a {}", name, age,gender);
     }
+    std::tuple<std::string,std::string,int> to_tuple() {
+        return std::make_tuple(name,gender,age);
+    }
 };
+
+auto add14=[](auto x,auto y){
+    return x+y;
+};
+
+auto add20(auto x,auto y) {
+    return x+y;
+}
+
+
+
 
 int main() {
     Student s{"john","boy",22};
     s.introduce();
+    auto [x,y,z] = s.to_tuple();
+    std::println("age is {}",z);
+    auto a=add14(11,25.1);
+    auto b=add20(11,25.1);
+    std::println("{} {}",a,b);
+
+
+
 }
--- code/oct25/test.cpp
+++ code/oct25/test.cpp
@@ -2,7 +2,55 @@
 // Created by guan on 2025/9/18.
 //
 #include <print>
-
-int main() {
-    std::println("Hello World");
+#include <map>
+#include <iostream>
+template<typename T,typename ...Args>
+void my_print(T t,Args... a) {
+    std::print("{}  ",t);
+    if constexpr(sizeof...(a)>0) {
+        my_print(a...);
+    }
 }
+
+
+template<typename ...Args>
+void you_print(Args... a) {
+    (std::print("{}  ",a),...);
+}
+
+
+template<typename ...Args>
+auto average(Args ...a) {
+    if constexpr(sizeof...(a)==0) {
+        return 0.0;
+    }else {
+        return static_cast<double>((a+...))/sizeof...(a);
+    }
+}
+
+
+template <typename Key, typename Value, typename F>
+void update(std::map<Key, Value>& m, F foo) {
+    for (auto&[k,v]:m) v=foo(k);
+
+}
+int main() {
+    // std::map<std::string, long long int> m {
+    // {"a", 1},
+    // {"b", 2},
+    // {"c", 3}
+    // };
+    // update(m, [](std::string key) {
+    // return std::hash<std::string>{}(key);
+    // });
+    // for (auto&& [key, value] : m)
+    //     std::cout << key << ":" << value << std::endl;
+    double res=average();
+    std::println("{}",res);
+}
+
+// int main() {
+//     my_print("hello",2,34,"uu",'a');
+//     std::println();
+//     you_print(2,34,"uu",'a');
+// }
