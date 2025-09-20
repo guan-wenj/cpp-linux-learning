446ef3ccc9730db491d1c1a51a11a3dd5c419a19
guan <guan.wenj@qq.com>
2025-09-20 15:13:38 +0800

    9.20学习

---
+++ code/oct25/play_function.cpp
@@ -0,0 +1,36 @@
+//
+// Created by guan on 2025/9/20.
+//
+
+#include <functional>
+#include <print>
+#include <iostream>
+auto average(int x,int y,int z,int e) {
+    return static_cast<double>(x+y+z+e)/4;
+}
+template<typename T>
+auto make(T&& t) {
+    std::println("right value");
+}
+
+
+template<typename T>
+auto make(T& t) {
+    std::println("left value");
+}
+
+int main() {
+    // auto a=std::bind(average,std::placeholders::_1,std::placeholders::_2,4,4);
+    // double b=a(4,8);
+    // std::println("{}",b);
+
+    std::string s="hello";
+    make(s);
+    make("hello");
+    make(std::move(s));
+    make(std::string("hello"));
+    make(std::forward<std::string>(s));
+    int n;
+
+
+}
